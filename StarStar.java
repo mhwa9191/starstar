58339cc9d9675636cf35577d6f537517bb5b4ac7
mhwa9191 <mmhhwa@gmail.com>
2022-10-26 09:13:14 +0900

    starstar level2

--- StarStar.java
+++ StarStar.java
@@ -5,5 +5,14 @@ for (int i=1;i<6;i++){
 	System.out.print("*");
 	}System.out.println(" ");}
 System.out.println("================");
+
+for(int i=1;i<6;i++){
+	for (int j=0; j<i;j++){
+	System.out.print("*");
+	}System.out.println();}
+System.out.println("================");
+
+
+
 } 
 }
